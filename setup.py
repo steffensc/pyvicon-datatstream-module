849fd34de90b291198814092403161afaef82242
Steffen <schmelter.steffen@googlemail.com>
2023-03-29 19:53:22 +0200

    increase to version v0.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 setup(
     name="pyvicon_datastream",
-    version="0.2.1",
+    version="0.2.2",
     description="Platform independent Python3 wrapper for Vicon Datastream SDK",
     author="Steffen Schmelter",
     packages=["pyvicon_datastream"],
